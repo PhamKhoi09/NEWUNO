8b7530f04f9dca30d3b6e92cf40a5b45e73f7776
PhamKhoi09 <phamkhoi090905@gmail.com>
2024-12-22 14:49:10 +0700

    Update BOBAI.cs

--- UNOServer/UNOServer/BOBAI.cs
+++ UNOServer/UNOServer/BOBAI.cs
@@ -10,7 +10,7 @@ namespace UNOServer
     internal class BOBAI
     {
         public static string currentCard = "";
-        public static string[] CardName =
+        public static string[] DefaultCardName =
         {
                 "Red_0", "Red_1", "Red_2", "Red_3", "Red_4", "Red_5", "Red_6", "Red_7", "Red_8", "Red_9", "Red_1_", "Red_2_", "Red_3_", "Red_4_", "Red_5_", "Red_6_", "Red_7_", "Red_8_", "Red_9_",
                 "Blue_0", "Blue_1", "Blue_2", "Blue_3", "Blue_4", "Blue_5", "Blue_6", "Blue_7", "Blue_8", "Blue_9", "Blue_1_", "Blue_2_", "Blue_3_", "Blue_4_", "Blue_5_", "Blue_6_", "Blue_7_", "Blue_8_", "Blue_9_",
@@ -21,7 +21,13 @@ namespace UNOServer
                 "Wild_1", "Wild_2", "Wild_3", "Wild_4",
                 "Wild_Draw_1", "Wild_Draw_2", "Wild_Draw_3", "Wild_Draw_4",
                 "Blue_Draw", "Blue_Draw_", "Red_Draw", "Red_Draw_", "Yellow_Draw", "Yellow_Draw_", "Green_Draw", "Green_Draw_"
-        }; //Mảng string chứa tên các lá bài trong bộ bài
+        }; //Mảng string gốc chứa tên các lá bài trong bộ bài
+        public static string[] CardName;
+        // Hàm khởi tạo lại CardName
+        public static void ResetCardName()
+        {
+            CardName = (string[])DefaultCardName.Clone();
+        }
     }
     /* Class để lưu trữ các bài đã mở ra (đã đánh) */
     internal class MoBai
